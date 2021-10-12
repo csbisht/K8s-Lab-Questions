a57579cc4e6a5a3961f500e5a4d23d5ac120dcdd
csbisht1@gmail.com <csbisht1@gmail.com>
2021-10-12 15:39:14 +0000

    add scripts

--- CKA_Lab1/qsn_check/Q1.sh
+++ CKA_Lab1/qsn_check/Q1.sh
@@ -10,14 +10,12 @@ out1="$?"
 check2=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get pod "$podname" -o jsonpath='{.spec.containers[*].securityContext.capabilities.add}' |grep -w "$systime"`
 out2="$?"
 
-checkpod=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get pod "$podname" |grep -w "$podname" 2>/dev/null`
+checkpod=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get pod "$podname" |grep -w "$podname"`
 out3="$?"
 
 if [ "${out3}" = 0 ]; then
 if [ "${out1}" -gt 0 ] && [ "${out2}" -gt 0 ]; then
 echo "command or capabilities not found in pod"
-#else
-#echo "command and capabilities found in pod"	
 fi
 else
 echo "pod "$podname" not found on "$1""
