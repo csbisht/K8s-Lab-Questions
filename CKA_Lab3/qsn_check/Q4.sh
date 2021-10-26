d3d557452d42172121da3fc43a6750913841d4e8
csbisht1@gmail.com <csbisht1@gmail.com>
2021-10-26 08:13:39 +0000

    add script

--- CKA_Lab3/qsn_check/Q4.sh
+++ CKA_Lab3/qsn_check/Q4.sh
@@ -3,21 +3,23 @@
 deploymentname="nginx-web"
 clstnum=`echo "${1}" |cut -d'r' -f2`
 imagename="nginx:1.19"
+filename="Rollout_history_cluster"
+filepath="/opt/K8sLab/Lab"
 
 checkdeployment=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get deployment "${deploymentname}""${clstnum}"`
 out3="$?"
 
 if [ "${out3}" = 0 ]; then
-checkrecord=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config rollout history deployment/"${deploymentname}""${clstnum}" |grep -w "nginx=${imagename}"`
-out1="$?"
 
-if [ "${out1}" = 0 ]; then
+if [ -f "$filepath""${clstnum}"/"$filename""${clstnum}".txt ]; then
+gethistory=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config rollout history deployment "${deploymentname}""${clstnum}" |sed '1,2d' |awk {'print $1'}`
+catfile=`cat "$filepath""${clstnum}"/"$filename""${clstnum}".txt |sed '1,2d' |awk {'print $1'}`
+if [ "$gethistory" = "$catfile" ]; then
+
 checkimage=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get deployment "${deploymentname}""${clstnum}" -o yaml |grep -w "image: ${imagename}"`
 out2="$?"
-
 if [ "${out2}" = 0 ]; then
 checkreplica=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get pods |grep "${deploymentname}${clstnum}" |wc -l`
-
 if [ "${checkreplica}" -lt 3 ]; then
 echo "replicas not scaled to 3"
 out3="1"
@@ -25,15 +27,17 @@ else
 echo "replicas scaled to 3"
 out3="0"	
 fi
-
 else
-echo "deploy image not found"
+echo "upgrade deploy image not found"
 out3="1"	
 fi
 
 else
-echo "rollout history not found"	
-out3="1"
+echo "Rollout history not matched with file "$filepath""${clstnum}"/"$filename""${clstnum}".txt"
+fi
+
+else
+echo "file "$filepath""${clstnum}"/"$filename""${clstnum}".txt not found on "$1""
 fi
 
 else
