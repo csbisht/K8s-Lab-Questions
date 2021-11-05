b36732ca39856c476a0c235a16fb6defd58b493f
csbisht1@gmail.com <csbisht1@gmail.com>
2021-11-05 10:34:21 +0000

    add script

---
+++ CKA_Lab3/qsn_check/Q18.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+deployname="web-nginx"
+filelocation="/opt/K8sLab/Lab3"
+clstnum=`echo ${1} |cut -d'r' -f2`
+
+
+if [ -f "${filelocation}"/ClusterIP-"${1}".svc ]; then
+checksvc=`cat "${filelocation}"/ClusterIP-"${1}".svc |grep -w "${deployname}""${clstnum}"-svc.default.svc.cluster.local`
+out3="$?"
+
+if [ "${out3}" = 0 ]; then
+echo "file "${filelocation}"/ClusterIP-"${1}".svc output matched"
+out3="0"
+else
+echo "file "${filelocation}"/ClusterIP-"${1}".svc output not matched"
+out3="1"
+fi
+
+else
+echo "file "${filelocation}"/ClusterIP-"${1}".svc not found"
+out3="1"
+fi
+
