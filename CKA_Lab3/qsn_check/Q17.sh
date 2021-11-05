2f2ad7b4ae0ee461f58b01d6be7f81464dfce1a6
csbisht1@gmail.com <csbisht1@gmail.com>
2021-11-05 07:22:24 +0000

    add script

---
+++ CKA_Lab3/qsn_check/Q17.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+podname="nginx-label"
+labelname="app=v1"
+labelrm="app"
+clstnum=`echo ${1} |cut -d'r' -f2`
+
+checkpod=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get pod "$podname""$clstnum" |grep -w "$podname""$clstnum"`
+out3="$?"
+
+if [ "${out3}" = 0 ]; then
+checklabel=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get  pods --show-labels |grep -w "$labelrm"`
+out1="$?"	
+
+if [ "${out1}" -gt 0 ]; then
+echo "Label "$labelname""$clstnum" removed from pod "$podname""$clstnum" on "$1""
+out3="0"
+else
+echo "Label "$labelname""$clstnum" not removed from pod "$podname""$clstnum" on "$1""
+out3="1"	
+fi
+
+else
+echo "pod "$podname""$clstnum" not found on "$1""
+out3="1"
+fi
