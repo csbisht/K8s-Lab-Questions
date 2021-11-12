bb0c521160675e03598666ee5b7cf7e2ed801837
csbisht1@gmail.com <csbisht1@gmail.com>
2021-11-12 11:45:34 +0000

    update script

--- CKA_Lab2/qsn_check/Q13.sh
+++ CKA_Lab2/qsn_check/Q13.sh
@@ -5,12 +5,13 @@ svcname="web-front-svc"
 filelocation="/opt/K8sLab/Lab2"
 
 if [ -f "${filelocation}"/"${podname}"-"${1}".svc ]; then
-checksvc=`cat "${filelocation}"/"${podname}"-"${1}".svc |grep -w web-app-svc.default.svc.cluster.local`
+checksvc=`cat "${filelocation}"/"${podname}"-"${1}".svc |grep -w "$svcname".default.svc.cluster.local`
 out3="$?"
 
 if [ "${out3}" = 0 ]; then
+	
 if [ -f "${filelocation}"/"${podname}"-"${1}".pod ]; then
-checkpod=`cat "${filelocation}"/"${podname}"-"${1}".pod |grep -w web-app-svc.default.svc.cluster.local`
+checkpod=`cat "${filelocation}"/"${podname}"-"${1}".pod |grep -w default.pod.cluster.local`
 out1="$?"
 
 if [ "${out1}" = 0 ]; then
@@ -22,9 +23,13 @@ out3="1"
 fi
 
 else
-echo "file "${filelocation}"/"${podname}"-"${1}".svc output not matched"
+echo "file "${filelocation}"/"${podname}"-"${1}".pod not found"
 out3="1"
 fi
+
+else
+echo "file "${filelocation}"/"${podname}"-"${1}".svc output not matched"
+out3="1"	
 fi
 
 else
