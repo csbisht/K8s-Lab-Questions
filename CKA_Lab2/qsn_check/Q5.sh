c6e279e3c4607a965cafa20ccfe7572cf549af83
csbisht1@gmail.com <csbisht1@gmail.com>
2021-11-12 07:25:21 +0000

    update script

--- CKA_Lab2/qsn_check/Q5.sh
+++ CKA_Lab2/qsn_check/Q5.sh
@@ -3,15 +3,16 @@
 podname="static-pod"
 nodename="controlplane"
 clstnum=`echo ${1} |cut -d'r' -f2`
+static-pod-cluster1-controlplane
 
-checkstaticpod=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get pods "$podname"-"$nodename"`
+checkstaticpod=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get pods "$podname"-"$1"-"$nodename"`
 out3="$?"
 
 
 
 if [ "${out3}" = 0 ]; then
-echo "static pod "$podname"-"$nodename" found in cluster $1"
+echo "static pod "$podname"-"$1"-"$nodename" found in cluster $1"
 else
-echo "static pod "$podname"-"$nodename" not found in cluster $1"
+echo "static pod "$podname"-"$1"-"$nodename" not found in cluster $1"
 out3="1"
 fi
