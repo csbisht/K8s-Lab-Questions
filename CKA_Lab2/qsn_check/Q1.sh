5434ce5bb327ef9cebf76b4fe3a274b803a2f721
csbisht1@gmail.com <csbisht1@gmail.com>
2021-11-12 06:17:09 +0000

    update script

--- CKA_Lab2/qsn_check/Q1.sh
+++ CKA_Lab2/qsn_check/Q1.sh
@@ -4,28 +4,39 @@ deploymentname="web-00"
 clstnum=`echo "${1}" |cut -d'r' -f2`
 imagename="nginx:1.18"
 
+checkdeployment=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get deployment "${deploymentname}""${clstnum}"`
+out3="$?"
+
+if [ "${out3}" = 0 ]; then
 checkrecord=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config rollout history deployment/"${deploymentname}""${clstnum}" |grep -w "nginx=${imagename}"`
 out1="$?"
 
+if [ "${out1}" = 0 ]; then
 checkimage=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get deployment "${deploymentname}""${clstnum}" -o yaml |grep -w "image: ${imagename}"`
 out2="$?"
 
+if [ "${out2}" = 0 ]; then
 checkreplica=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get pods |grep "${deploymentname}${clstnum}" |wc -l`
 
-checkdeployment=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get deployment "${deploymentname}""${clstnum}"`
-out4="$?"
-
-if [ "${out4}" = 0 ]; then
-if [ "${out1}" -gt 0 ] && [ "${out2}" -gt 0 ]; then
-echo "rollout history or deploy image not found"
-out3="1"
-else
-#echo "rollout history or deploy image found or replicas scaled to 3"	
 if [ "${checkreplica}" -lt 3 ]; then
 echo "replicas not scaled to 3"
 out3="1"
+else
+echo "replicas scaled to 3"
+out3="0"
 fi
+
+else
+echo "update image not found"
+out3="1"
 fi
+
+else
+echo "rollout history not found"
+out3="1"
+fi
+
+
 else
 echo "deployment "${deploymentname}""${clastnum}" not found on "$1""
 out3="1"
