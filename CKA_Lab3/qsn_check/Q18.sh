edd227d540229b914a70d09b93e37f5b09f57cfb
csbisht1@gmail.com <csbisht1@gmail.com>
2021-11-19 09:48:37 +0000

    update script

--- CKA_Lab3/qsn_check/Q18.sh
+++ CKA_Lab3/qsn_check/Q18.sh
@@ -6,7 +6,7 @@ clstnum=`echo ${1} |cut -d'r' -f2`
 
 
 if [ -f "${filelocation}"/ClusterIP-"${1}".svc ]; then
-checksvc=`cat "${filelocation}"/ClusterIP-"${1}".svc |grep -w "${deployname}""${clstnum}"-svc.default.svc.cluster.local`
+checksvc=`cat "${filelocation}"/ClusterIP-"${1}".svc |grep -w default.svc.cluster.local`
 out3="$?"
 
 if [ "${out3}" = 0 ]; then
