a73f99ee732b7cf264d51c4b86821bd68eeab7ce
csbisht1@gmail.com <csbisht1@gmail.com>
2021-11-28 14:43:23 +0000

    update script

--- CKA_Lab2/qsn_check/Q5.sh
+++ CKA_Lab2/qsn_check/Q5.sh
@@ -3,7 +3,6 @@
 podname="static-pod"
 nodename="controlplane"
 clstnum=`echo ${1} |cut -d'r' -f2`
-static-pod-cluster1-controlplane
 
 checkstaticpod=`/usr/bin/kubectl --kubeconfig=$HOME/K8s-Lab-Questions/kubeconfig/"$1".config get pods "$podname"-"$1"-"$nodename"`
 out3="$?"
